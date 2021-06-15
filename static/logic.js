96ac7dcbb01241b68a2ae41a5d397436a90f35ac
Jordan S <77416389+Jordan-0618@users.noreply.github.com>
2021-06-15 12:08:43 -0700

    push

--- static/logic.js
+++ static/logic.js
@@ -1,68 +1,89 @@
 // Creating map object
 var myMap
 var incomeLayer = L.geoJSON()
+var priceLayer = L.geoJSON()
+var priceMarkers = [];
+var incomeDict = {};
 
-function createMap(incomeLayer){
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+// Use this link to get the geojson data.
+var link = "static/data/Zip_Codes.geojson";
+
+d3.csv("static/data/yelp_academic_dataset_business.csv").then(function(yelpData) {
+  yelpData.forEach(function(data) {
+    var priceRange = data["attributes.Price Range"]
+    var rating = +data["stars"]
+    var lat = +data["latitude"]
+    var long = +data["longitude"]
+
+
+    // Loop through the stations array
+    // for (var index = 1; index < priceRange.length; index++) {
+      // var station = priceRange[index];
+
+    // For each station, create a marker and bind a popup with the station's name
+    var priceMarker = L.marker([lat, long])
+    .bindPopup("<h3>" + priceRange + "<h3><h3>Rating: " + rating + "</h3>");
+
+      // Add the marker to the bikeMarkers array
+    priceMarkers.push(priceMarker);
+    // };
+    // console.log(lat[1]);
+  //  console.log(priceRange)
+  });
+  console.log("OTHER TEST");
+
+  function createMap(incomeLayer, priceLayer){
+    var streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+    });
+  
+    var darkMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "dark-v10",
+        accessToken: API_KEY
+    }); 
+  
+    var baseMaps = {
+        "Street Map": streetMap,
+        "Dark Map": darkMap
+    };
+    // Adding tile layer
+  
+    var overlayMaps = {
+      incomeLayer: incomeLayer,
+      priceLayer: priceLayer 
+    };
+   
+  
+    myMap = L.map("map", {
+      center: [36.1699, -115.1398],
+      zoom: 11,
+      layers: [darkMap, streetMap, priceLayer, incomeLayer]
+    });
+  
+    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
       tileSize: 512,
       maxZoom: 18,
       zoomOffset: -1,
       id: "mapbox/streets-v11",
       accessToken: API_KEY
-  });
-
-  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "dark-v10",
-      accessToken: API_KEY
-  }); 
-
-  var baseMaps = {
-      "Street Map": streetmap,
-      "Dark Map": darkmap
+    }).addTo(myMap);
+  
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
   };
-  // Adding tile layer
-
-  var overlayMaps = {
-    incomeLayer: incomeLayer 
-  };
- 
-
-  myMap = L.map("map", {
-    center: [36.1699, -115.1398],
-    zoom: 11
-  });
-
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
-
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
-};
-// // Create a legend to display info
-// var legend = L.control({
-//   position: "bottomright"
-// });
-// // When layer control is added insert a div with class legend
-// legend.onAdd = function() {
-//   var div = L.DomUtil.create("div", "legend");
-//   return div;
-// };
-// //Add info to map
-// legend.addTo(myMap);
-
-  // Use this link to get the geojson data.
-var link = "static/data/Zip_Codes.geojson";
-
+  // console.log(priceMarkers) 
+  // priceLayer.addData(priceMarkers); 
+  createMap(incomeLayer, L.layerGroup(priceMarkers));
+});
 function chooseColor(income) {
   switch (true) {
   case income > 75000:
@@ -80,33 +101,8 @@ function chooseColor(income) {
   };
 }  
 
-// var legend = L.control({position: 'bottomright'});
-
-// legend.onAdd = function (myMap) {
-
-//     var div = L.DomUtil.create('div', 'info legend'),
-//         grades = ["$75K+", "$60K - $75K", "$50K - $59K", "$40K - $49K", "$30K - $39K", "$0 - $29K"],
-//         labels = [];
-
-//     // loop through our density intervals and generate a label with a colored square for each interval
-//     for (var i = 0; i < grades.length; i++) {
-//         div.innerHTML +=
-//             '<i style="background:' + chooseColor(grades[i] + 1) + '"></i> ' +
-//             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-//     }
-
-//     return div;
-// };
-// console.log(legend);
-
-// legend.addTo(myMap);
-
-var incomeDict = {};
-
-createMap(incomeLayer);
-
 d3.json(link).then(function(data) {
-  d3.csv("Resources/kaggle_income.csv").then(function(incomeData) {
+  d3.csv("static/data/kaggle_income.csv").then(function(incomeData) {
     // Step 4: Parse the data
     
     // Format the data
@@ -126,7 +122,11 @@ d3.json(link).then(function(data) {
           fillOpacity: 0.8,
           weight: 1.5
         };
-       
     });
   });
+  console.log("TEST");
+  
 });
+
+
+
